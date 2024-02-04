3d68a0a11f77584fccb0942f7b249deb335e3ab6
Lyrise <lyrise1984@gmail.com>
2024-02-04 17:02:54 +0900

    fix

--- build-linux.sh
+++ build-linux.sh
@@ -6,4 +6,4 @@ rm -rf ./pub/linux-x64/*
 export NativeDepsPlatform=linux
 export PlatformTarget=x64
 
-dotnet publish ./src/ImageConverter -p:PublishSingleFile=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/
+dotnet publish ./src/ImageConverter -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/
--- build-win.ps1
+++ build-win.ps1
@@ -3,4 +3,4 @@ Remove-Item ./pub/win-x64/* -Recurse
 $Env:NativeDepsPlatform="Windows"
 $Env:PlatformTarget="x64"
 
-dotnet publish ./src/ImageConverter -p:PublishSingleFile=true --runtime win-x64 --configuration Release --self-contained true --output ./pub/win-x64/
+dotnet publish ./src/ImageConverter -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime win-x64 --configuration Release --self-contained true --output ./pub/win-x64/
